521c15f01d209efabd535fe96f8f9706416f1ae8
Hearmen <qince@sohu.com>
2017-10-10 21:31:44 +0800

    daliy update

--- js_tool/wasmbuilder.js
+++ js_tool/wasmbuilder.js
@@ -10,7 +10,17 @@ function getRandomInt(min, max) {
 function getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+function genRandomData() {
+        let junk = [];
+        let junkSize = getRandomInt(0,10000000);
+        for (let i=0; i< junkSize; i++)
+        {
+            junk.push(String.fromCharCode(getRandomInt(0,0x100)));
+        }
+        return junk;
 }
 
 function constructMemory(self) {
@@ -25,8 +35,9 @@ function constructMemory(self) {
 
 function constructStart(self) {
         let startId = getRandomInt(0, self.functions.length);
+        if (funcTable[startId].type != kSig_v_v) // start function signature must be sig_v_v
+          return;
         self.addStart(startId)
-        // Todo : start function signature must be sig_v_v 
 }
 
 function constructExplicitSection(self) {
@@ -52,14 +63,16 @@ function constructType(self, isAdd){
         return sig
 }
 
-function constructGlobal(self){
+function constructGlobal(self){    // Todo : Global init is 0 ,and can not set
         let mutable = getRandomIntInclusive(0,1);
         let globalType = globalTypes[getRandomInt(0, globalTypes.length)];
         let isExport = getRandomIntInclusive(0,1);
         let isExport ? self.addGlobal( globalType, mutable).exportAs('global'+(gExportCount++) ): self.addGlobal( globalType, mutable);
+        globalTable.push(globalType);
 }
 
 function constructBody(self) {   // WasmFunctionBuilder
+    
     self.addBody(data)    // anything you want
 }
 
@@ -71,14 +84,15 @@ function constructFunction(self) {
         let func = self.addFunction(fnName, type);
         constructBody(func);
         funcTable.push({'name':fnName,'type':type});
-        func.exportFunc()
+        func.exportFunc()    // export every function
 }
 
-// Todo : mix these function below, 
+// Todo : mix these function below,
 function constructImportFunc(self) {
         let sig = constructType(self, false);
         let importFnName =  'import'+(gImportCount++);
         self.addImport( 'mod', importFnName, sig);
+        funcTable.push({'name':importFnName, 'type':sig })
         importFuncTable.push({'name':importFnName, 'type':sig })
 }
 
@@ -106,20 +120,22 @@ function constructImportTable(self) {
         let maximum = getRandomInt(initial,65536);
         let importTableName = 'import'+(gImportCount++);
         self.addImportedTable('mod', importTableName, initial, maximum);
-        hasTable = true;   // import WebAssembly.Table anyway
+        self.setFunctionTableLength(initial);
+        gTableLength = length;
+        hasTable = true;
 }
 
 function constructImport(self) {
         switch (getRandomInt(0,3)) {
             case 0: constructImportFunc(self);break;
             case 1: constructImportGlobal(self);break;
-            case 2: 
-                if(hasMemory) 
+            case 2:
+                if(hasMemory)
                     constructImportFunc(self);
                 else
                     elseconstructImportMemory(self);
                 break;
-            case 3: 
+            case 3:
                 if(hasTable)
                     constructImportGlobal(self);
                 else
@@ -128,21 +144,82 @@ function constructImport(self) {
         }
 }
 
+function constructExportFunc(self) {
+    let index = getRandomInt(0,funcTable.length);
+    self.addExport(funcTable[index].name, index);
+}
+
+function constructExportGlobal(self) {
+    let index = getRandomInt(0,globalTable.length);
+    self.addExportOfKind('exGlobal'+ (gExGlobal++), kExternalGlobal,index);
+}
+
+function constructExportTable(self) { // Table & Memory
+    let index = 0;
+    self.addExportOfKind('exTable', kExternalTable, index);
+}
+
+function constructExportMemory(self) {
+    let index = 0;
+    self.addExportOfKind('exMemory', kExternalMemory, index);
+}
+
 function constructExport(self) {
-    switch() {
-        case 0:
+    switch(getRandomInt(0,3)) {
+        case 0: constructExportFunc(self);break;
+        case 1: constructExportGlobal(self);break;
+        case 2: constructExportTable(self);break;
+        case 3: constructExportMemory(self);break;
     }
+}
+
+function constructDate(self) {
+    let isGlobal = getRandomInt(0,1);
+    if (isGlobal)
+        let addr = getRandomInt(0,globalTable.length);
+    else
+        let addr = getRandomInt(0,16384*0x1000);
+    let data = genRandomData();
+    self.addDataSegment(addr, data, isGlobal);
+}
+
+function constructElement(self) {
+    if( !hasTable )
+        return;
+    let isGlobal = getRandomInt(0,1);
+    let isImport = getRandomInt(0,1);
+    if (isGlobal)
+        let base = getRandomInt(0,globalTable.length);
+    else
+        let base = getRandomInt(0,gTableLength);  // table current length
+    let segLength = getRandomInt(0, 10000000);  // MAX table size 10000000
+    let initValueArray = [];
+    for (let i =0; i< segLength; i++)
+    {
+        initValueArray.push(getRandomInt(0, funcTable.length));
+    }
+    self.addFunctionTableInit(base, isGlobal, initValueArray, isImport)
+}
+
+function constructTable(self) {
+    if (hasTable)
+      return;
+    let size = getRandomInt(0, 10000000);
+    gTableLength = length;
+    self.setFunctionTableLength(length);   // Table initial ,maximum equal initial
+    hasTable = true;
 }
 
 var builder = new WasmModuleBuilder();
 
 const globalTypes = [kWasmI32 , kWasmI64, kWasmF32, kWasmF64];  // Todo : wbGetGlobal
 const localTypes = [kWasmI32 , kWasmI64, kWasmF32, kWasmF64, kWasmS128];
-const constructors = ['addStart','addMemory','addExplicitSection','addCustomSection','addType','addGlobal','addFunction','addImport'];
+const constructors = ['addStart','addMemory','addExplicitSection','addCustomSection','addType','addGlobal','addFunction','addImport','addExport','addDataSegment','addElementSegment'];
 const Opcodes = [kExprUnreachable ,kExprNop ,kExprBlock ,kExprLoop ,kExprIf ,kExprElse ,kExprTry ,kExprCatch ,kExprThrow ,kExprEnd ,kExprBr ,kExprBrIf ,kExprBrTable ,kExprReturn ,kExprCallFunction ,kExprCallIndirect ,kExprDrop ,kExprSelect ,kExprGetLocal ,kExprSetLocal ,kExprTeeLocal ,kExprGetGlobal ,kExprSetGlobal ,kExprI32Const ,kExprI64Const ,kExprF32Const ,kExprF64Const ,kExprI32LoadMem ,kExprI64LoadMem ,kExprF32LoadMem ,kExprF64LoadMem ,kExprI32LoadMem8S ,kExprI32LoadMem8U ,kExprI32LoadMem16S ,kExprI32LoadMem16U ,kExprI64LoadMem8S ,kExprI64LoadMem8U ,kExprI64LoadMem16S ,kExprI64LoadMem16U ,kExprI64LoadMem32S ,kExprI64LoadMem32U ,kExprI32StoreMem ,kExprI64StoreMem ,kExprF32StoreMem ,kExprF64StoreMem ,kExprI32StoreMem8 ,kExprI32StoreMem16 ,kExprI64StoreMem8 ,kExprI64StoreMem16 ,kExprI64StoreMem32 ,kExprMemorySize ,kExprGrowMemory ,kExprI32Eqz ,kExprI32Eq ,kExprI32Ne ,kExprI32LtS ,kExprI32LtU ,kExprI32GtS ,kExprI32GtU ,kExprI32LeS ,kExprI32LeU ,kExprI32GeS ,kExprI32GeU ,kExprI64Eqz ,kExprI64Eq ,kExprI64Ne ,kExprI64LtS ,kExprI64LtU ,kExprI64GtS ,kExprI64GtU ,kExprI64LeS ,kExprI64LeU ,kExprI64GeS ,kExprI64GeU ,kExprF32Eq ,kExprF32Ne ,kExprF32Lt ,kExprF32Gt ,kExprF32Le ,kExprF32Ge ,kExprF64Eq ,kExprF64Ne ,kExprF64Lt ,kExprF64Gt ,kExprF64Le ,kExprF64Ge ,kExprI32Clz ,kExprI32Ctz ,kExprI32Popcnt ,kExprI32Add ,kExprI32Sub ,kExprI32Mul ,kExprI32DivS ,kExprI32DivU ,kExprI32RemS ,kExprI32RemU ,kExprI32And ,kExprI32Ior ,kExprI32Xor ,kExprI32Shl ,kExprI32ShrS ,kExprI32ShrU ,kExprI32Rol ,kExprI32Ror ,kExprI64Clz ,kExprI64Ctz ,kExprI64Popcnt ,kExprI64Add ,kExprI64Sub ,kExprI64Mul ,kExprI64DivS ,kExprI64DivU ,kExprI64RemS ,kExprI64RemU ,kExprI64And ,kExprI64Ior ,kExprI64Xor ,kExprI64Shl ,kExprI64ShrS ,kExprI64ShrU ,kExprI64Rol ,kExprI64Ror ,kExprF32Abs ,kExprF32Neg ,kExprF32Ceil ,kExprF32Floor ,kExprF32Trunc ,kExprF32NearestInt ,kExprF32Sqrt ,kExprF32Add ,kExprF32Sub ,kExprF32Mul ,kExprF32Div ,kExprF32Min ,kExprF32Max ,kExprF32CopySign ,kExprF64Abs ,kExprF64Neg ,kExprF64Ceil ,kExprF64Floor ,kExprF64Trunc ,kExprF64NearestInt ,kExprF64Sqrt ,kExprF64Add ,kExprF64Sub ,kExprF64Mul ,kExprF64Div ,kExprF64Min ,kExprF64Max ,kExprF64CopySign ,kExprI32ConvertI64 ,kExprI32SConvertF32 ,kExprI32UConvertF32 ,kExprI32SConvertF64 ,kExprI32UConvertF64 ,kExprI64SConvertI32 ,kExprI64UConvertI32 ,kExprI64SConvertF32 ,kExprI64UConvertF32 ,kExprI64SConvertF64 ,kExprI64UConvertF64 ,kExprF32SConvertI32 ,kExprF32UConvertI32 ,kExprF32SConvertI64 ,kExprF32UConvertI64 ,kExprF32ConvertF64 ,kExprF64SConvertI32 ,kExprF64UConvertI32 ,kExprF64SConvertI64 ,kExprF64UConvertI64 ,kExprF64ConvertF32 ,kExprI32ReinterpretF32 ,kExprI64ReinterpretF64 ,kExprF32ReinterpretI32 ,kExprF64ReinterpretI64];
 
 var typeTable = [];
-var funcTable = [];
+var funcTable = [];   // local
+var globalTable = [];    // local
 var importFuncTable = [];
 var importGlobalTable = [];
 var hasMemory = false;
@@ -150,24 +227,26 @@ var hasTable = false;
 var gExportCount = 0;
 var gFuncCount = 0;
 var gImportCount = 0;
+var gExGlobalCount = 0;
+var gTableLength = 0;
 
 for( var cc = 0; cc < getRandomInt(1, 1000); cc++)
 {
-        componentIndex =  getRandomIntInclusive(0,16);
+        componentIndex =  getRandomIntInclusive(0,12);
 
         switch(componentIndex)
         {
                 case 0:
-                        constructStart(builder)    // addStart(start_index) 
+                        constructStart(builder)    // addStart(start_index)
                         break;
                 case 1:
-                        constructMemory(builder)  // addMemory(min, max, exported)  
+                        constructMemory(builder)  // addMemory(min, max, exported)
                         break;
                 case 2:
-                        constructExplicitSection(builder) // addExplicitSection(bytes)  anything you like 
+                        //constructExplicitSection(builder) // addExplicitSection(bytes)  anything you like
                         break;
                 case 3:
-                        constructCustomSection(builder) // addCustomSection(name, bytes)
+                        //constructCustomSection(builder) // addCustomSection(name, bytes)  Chakra have not parse this section
                         break;
                 case 4:
                         constructType(builder, true) //addType(type)
@@ -179,10 +258,19 @@ for( var cc = 0; cc < getRandomInt(1, 1000); cc++)
                         constructFunction(builder) // addFunction(name, type)
                         break;
                 case 7:
-                        constructImport(builder)// addImport(module = "", name, type); addImportedGlobal(module = "", name, type); addImportedMemory(module = "", name, initial = 0, maximum); addImportedTable(module = "", name, initial, maximum)
+                        constructImport(builder) // addImport(module = "", name, type); addImportedGlobal(module = "", name, type); addImportedMemory(module = "", name, initial = 0, maximum); addImportedTable(module = "", name, initial, maximum)
                         break;
                 case 8:
-                        constructExport(builder) 
+                        constructExport(builder) // addExport(name, index); addExportOfKind(name, kind, index)
+                        break;
+                case 9:
+                        constructData(builder); // addDataSegment(addr, data, is_global = false)
+                        break;
+                case 10:
+                        constructElement(builder)  // addFunctionTableInit(base, is_global, array, is_import = false)
+                        break;
+                case 11:
+                        constructTable(builder) // setFunctionTableLength(length)
                         break;
         }
 }
